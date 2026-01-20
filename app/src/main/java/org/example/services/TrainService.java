2c231da40ed11299938c4d1bf5fc748a2e4bfe1d
hassansherwani10 <hassansherwani633@gmail.com>
2026-01-20 14:51:38 +0500

    seventh commit

---
+++ app/src/main/java/org/example/services/TrainService.java
@@ -0,0 +1,81 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.Train;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TrainService {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<Train> trainList;
+    private static final String TRAIN_FINAL_PATH = "app/src/main/java/org/example/app/localDb/trains.json";
+
+    public TrainService() throws IOException{
+        File trains = new File(TRAIN_FINAL_PATH);
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>(){});
+    }
+
+    // To save the changes in file (Local DB)
+    private void saveTrainListToFile(){
+        try{
+            objectMapper.writeValue(new File(TRAIN_FINAL_PATH) , trainList); // To save date of "trainList" in file (Local DB)
+        }
+        catch (IOException exception){
+            System.out.println("Unable to save train data. Please try again later.");
+        }
+    }
+
+    // This checks whether the TRAIN's go the source and destination input by USER
+    private boolean validTrain(Train train , String source , String destination){
+        List<String> stations = train.getStations().stream().map(String::toLowerCase).toList();
+
+        int sourceIndex = stations.indexOf(source.toLowerCase());
+        int destinationIndex = stations.indexOf(destination.toLowerCase());
+
+        return sourceIndex >= 0 && destinationIndex >= 0 && sourceIndex < destinationIndex;
+    }
+
+    // This tells us that the train is valid to go the desired source and destination
+    public List<Train> searchTrains(String source , String destination){
+        return trainList.stream().filter(train -> validTrain(train, source, destination)).collect(Collectors.toList());
+    }
+
+    // To add a New Train in our Data in our file (Local DB)
+    public void addTrain(Train newTrain){
+        Optional<Train> trainExists = trainList.stream().filter(train -> train.getTrainId().equalsIgnoreCase(newTrain.getTrainId())).findFirst();
+
+        if (trainExists.isPresent()){
+            // If a train with the same "trainId" exists, update it instead of adding a new one
+            updateTrain(newTrain);
+        }
+        else {
+            // Otherwise, add the new train to the list
+            trainList.add(newTrain);
+            saveTrainListToFile();
+        }
+    }
+
+    // To update the Data of already existing Train
+    public void updateTrain(Train updatedTrain){
+        // Find the index of the train with the same trainId
+        OptionalInt indexOfTrain = IntStream.range(0 , trainList.size()).filter(train -> trainList.get(train).getTrainId().equalsIgnoreCase(updatedTrain.getTrainId())).findFirst();
+
+        if (indexOfTrain.isPresent()){
+            // If found, replace the existing train with the updated one
+            trainList.set(indexOfTrain.getAsInt() , updatedTrain);
+            saveTrainListToFile();
+        }
+        else {
+            // If not found, treat it as adding a new train
+            addTrain(updatedTrain);
+        }
+    }
+}
---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,168 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.Train;
+import org.example.entities.User;
+import org.example.utils.PasswordHashUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserBookingService {
+    private ObjectMapper objectMapper = new ObjectMapper(); // Converts JSON to Java OR JSON reader/writer
+    private List<User> userList; // List of all users
+    private User user; // To set user GLOBALLY
+
+    private static final String USER_FILE_PATH = "app/src/main/java/org/example/app/localDb/users.json"; // Path of "users.json" - LOCAL DB
+
+    // To load USERS from file
+    private void loadUsersFromFile() throws IOException {
+        userList = objectMapper.readValue(new File(USER_FILE_PATH), new TypeReference<List<User>>(){});
+    }
+
+    // To save "userList" in file
+    private void savesUserListToFile() throws IOException {
+        File usersFile = new File(USER_FILE_PATH);
+        objectMapper.writeValue(usersFile , userList);
+    }
+    
+    public UserBookingService(User user) throws IOException{
+        this.user = user;
+        loadUsersFromFile();
+    }
+
+    public UserBookingService() throws IOException {
+        loadUsersFromFile();
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(u -> {
+            return u.getName().equals(user.getName()) && PasswordHashUtil.checkPassword(user.getPassword() , u.getHashedPassword());
+        }).findFirst();
+
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUpUser(User newUser){
+        try{
+            boolean userExistsAlready = userList.stream().anyMatch(u -> u.getName().equalsIgnoreCase(newUser.getName())); // Checking the "newUser" name while Ignoring Letter Case
+            if (userExistsAlready){
+                System.out.println("User already exists.");
+                return Boolean.FALSE;
+            }
+
+            String hashedPassword = PasswordHashUtil.hashPassword(newUser.getPassword()); // To hash the user entered Plain Password
+            newUser.setHashedPassword(hashedPassword); // To set Hashed Password in User Class
+            newUser.setPassword(null); // To set Plain Password as NULL in User Class
+
+            userList.add(newUser); // Save all data of NEW USER in "userList"
+
+            savesUserListToFile(); // Again save that "userList" in "users.json" File
+
+            return Boolean.TRUE;
+        }
+        catch (IOException exception){
+            System.out.println("Error while signing up user.");
+            return Boolean.FALSE;
+        }
+    }
+
+    // To fetch Seats of Train
+    public List<List<Integer>> fetchSeats(Train train){
+        return train.getSeats();
+    }
+
+    // Show all booked tickets of the currently logged-in user
+    public void fetchBookings(){
+        Optional<User> userFetched = userList.stream().filter(u -> {
+            return u.getName().equals(user.getName()) && PasswordHashUtil.checkPassword(user.getPassword() , u.getHashedPassword());
+        }).findFirst();
+
+        if (userFetched.isPresent()){
+            userFetched.get().printTickets();
+        }
+        else {
+            System.out.println("Invalid credentials. Cannot fetch bookings.");
+        }
+    }
+
+    // To cancel the ticket with the help of "ticketId" and save changes in the file (Local DB)
+    public boolean cancelBooking(String ticketId){
+        if (ticketId == null || ticketId.isEmpty()){ // To check if "ticketId" is null or empty
+            System.out.println("Ticket ID cannot be null or empty.");
+            return false;
+        }
+
+        // Find the logged-in user in the "userList"
+        Optional<User> findLoggedInUser = userList.stream().filter(u ->
+            u.getName().equals(user.getName()) && PasswordHashUtil.checkPassword(user.getPassword() , u.getHashedPassword())
+        ).findFirst();
+
+        // If Logged-in user is not found
+        if (!findLoggedInUser.isPresent()){
+            System.out.println("User not found.");
+            return false;
+        }
+
+        User loggedInUser = findLoggedInUser.get(); // If Logged-in user is found
+
+        // Remove that Logged-in user ticket with help of "ticketId"
+        boolean removeTicket = loggedInUser.getTicketsBooked().removeIf(ticket -> ticket.getTicketId().equals(ticketId));
+
+
+        if (removeTicket){ // If ticket is successfully removed
+            try{ // Try to save changes in the file (Local DB)
+                savesUserListToFile(); // Save the changes in the file (Local DB)
+                System.out.println("Ticket with ID: " + ticketId + " - has been canceled.");
+                return true;
+            }
+            catch (IOException exception){ // If changes is not saved in the file (Local DB)
+                System.out.println("Failed to update booking data.");
+                return false;
+            }
+        }
+        else { // If ticket is not removed
+            System.out.println("No ticket found with ID: " + ticketId);
+            return false;
+        }
+    }
+
+    public List<Train> getTrains(String source , String destination){
+        try {
+            TrainService trainServiceObject = new TrainService();
+            return trainServiceObject.searchTrains(source, destination);
+        }
+        catch (IOException exception){
+            return new ArrayList<>();
+        }
+    }
+
+    // To book the seat in the TRAIN
+    public Boolean bookTrainSeat(Train train, int row, int seat){
+        try{
+            TrainService trainServiceObject = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()){
+                if (seats.get(row).get(seat) == 0){ // If seat of that row is equal to 0-(ZERO)
+                    seats.get(row).set(seat , 1); // then set that seat of row is equal to 1-(ONE)
+                    train.setSeats(seats); // To set new SEATS after booking
+                    trainServiceObject.addTrain(train); // To save this train properly in file - (Local DB)
+                    return true; // Booking successful
+                }
+                else {
+                    return false; // Seat is already booked
+                }
+            }
+            else {
+                return false; // Invalid row or seat index
+            }
+        }
+        catch (IOException exception){
+            return Boolean.FALSE;
+        }
+    }
+}
